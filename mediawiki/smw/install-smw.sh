7f96f5b078c7b8e74356e348a4be76cd3110e9db
Shelby Jueden <shelby@jueden.org>
2024-08-04 14:02:07 -0700

    Attempt to install QRSite that failed

--- mediawiki/smw/install-smw.sh
+++ mediawiki/smw/install-smw.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 if [ -f "/var/www/html/LocalSettings.php" ] && [ ! -f /var/www/html/composer.local.json ]; then
-    echo '{"require":{"mediawiki/semantic-media-wiki":"^4.2.0"}}' > /var/www/html/composer.local.json
+    echo '{"require":{"mediawiki/semantic-media-wiki":"^4.2.0"},
+    "extra": {"merge-plugin": {"include": ["extensions-custom/*/composer.json"]}}
+}' > /var/www/html/composer.local.json
     composer update --no-dev
 fi
 if [ -f "/var/www/html/LocalSettings.php" ] && ! grep -q "SemanticMediaWiki" /var/www/html/LocalSettings.php; then
