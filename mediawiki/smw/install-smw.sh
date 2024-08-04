8d7c7a1d33a6785d98ff739a35ee641e5e303295
Shelby Jueden <shelby@jueden.org>
2024-08-04 08:31:28 -0700

    Remove vars from sh

--- mediawiki/smw/install-smw.sh
+++ mediawiki/smw/install-smw.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-if [ -f "${MW_BASE_PATH}/${MW_LOCAL_SETTINGS}" ] && [ ! -f ${MW_BASE_PATH}/${MW_COMPOSER_LOCAL} ]; then
-    echo '{"require":{"mediawiki/semantic-media-wiki":"${SMW_VERSION}"}}' > ${MW_BASE_PATH}/${MW_COMPOSER_LOCAL}
+if [ -f "/var/www/html/LocalSettings.php" ] && [ ! -f /var/www/html/composer.local.json ]; then
+    echo '{"require":{"mediawiki/semantic-media-wiki":"^4.2.0"}}' > /var/www/html/composer.local.json
     composer update --no-dev
 fi
-if [ -f "${MW_BASE_PATH}/${MW_LOCAL_SETTINGS}" ] && ! grep -q "SemanticMediaWiki" ${MW_BASE_PATH}/${MW_LOCAL_SETTINGS}; then
-    echo "wfLoadExtension( 'SemanticMediaWiki' );" >> ${MW_BASE_PATH}/${MW_LOCAL_SETTINGS}
-    echo "enableSemantics( '${SMW_WIKI_DOMAIN}' );" >> ${MW_BASE_PATH}/${MW_LOCAL_SETTINGS}
+if [ -f "/var/www/html/LocalSettings.php" ] && ! grep -q "SemanticMediaWiki" /var/www/html/LocalSettings.php; then
+    echo "wfLoadExtension( 'SemanticMediaWiki' );" >> /var/www/html/LocalSettings.php
+    echo "enableSemantics( 'LOCALHOST' );" >> /var/www/html/LocalSettings.php
 fi
-/usr/local/bin/php ${MW_BASE_PATH}/maintenance/update.php
+/usr/local/bin/php /var/www/html/maintenance/update.php
